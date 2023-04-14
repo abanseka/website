734de9669b2dd8acbe4f383bb24e92d96a26a4d0
sly <sylvester@archis.company>
2023-04-14 19:19:26 +0100

    Fix failing build

--- pages/notes/notes.tsx
+++ pages/notes/notes.tsx
@@ -1,7 +1,7 @@
 import PostCard from "@components/widgets/postCard";
 
 export default function Notes({ allNotesData }: { allNotesData: any }) {
-  return allNotesData.map(
+  return allNotesData?.map(
     ({ date, title }: { date: string; title: string }) => (
       <PostCard key={title} title={title} date={date} />
     )
