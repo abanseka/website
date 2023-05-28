b33f7a49628e60dbbf7a9589efb8751e31d689e6
sly <sylvester@archis.company>
2023-05-28 14:52:52 +0100

    fix: breaking build

--- pages/works/index.tsx
+++ pages/works/index.tsx
@@ -1,8 +1,40 @@
 import Head from "next/head";
 import Link from "next/link";
 import { WorkCardProps } from "types/common";
-import { workData } from "./workData";
+import SiteLinkIcon from "@assets/link.svg";
+import RepoLinkIcon from "@assets/codelink.svg";
+import Villa from "@assets/villa.svg";
+import Shield from "@assets/shield.svg";
 import style from "./work.module.scss";
+
+const workCardImageStyle = {
+  width: "5rem",
+  height: "5rem",
+};
+
+export const workData: WorkCardProps[] = [
+  {
+    workTitle: "Personal Webstite",
+    workDescription: "My Corner of the internet",
+
+    siteLink: "https://abanseka.vercel.app/",
+    repoLink: "https://github.com/abanseka/website",
+
+    codeLinkIcon: <RepoLinkIcon />,
+    headerImage: <Villa style={workCardImageStyle} />,
+  },
+  {
+    workTitle: "Viginere Cipher",
+    workDescription: "A mini ecryption application ",
+
+    siteLink: "https://viginerecipher-begonia-9b5138.netlify.app/",
+    repoLink: "https://github.com/abanseka/viginere-cipher",
+
+    siteLinkIcon: <SiteLinkIcon />,
+    codeLinkIcon: <RepoLinkIcon />,
+    headerImage: <Shield style={workCardImageStyle} />,
+  },
+];
 
 const WorkCard = (work: WorkCardProps) => (
   <article className={style.workcard}>
