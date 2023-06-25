b4ba9a7589d9bbacb1729cbfb823a8d83d9aa08c
sly <sylvester@archis.company>
2023-06-25 04:35:39 +0100

    update: add handwritten font-face for note

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,6 +14,10 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@300;400;600;700&display=swap"
           rel="stylesheet"
         />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Satisfy&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <svg className="noisePattern">
