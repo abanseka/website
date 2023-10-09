b84cce9666d58782d111041ce4a4e2a36c2679d5
sly <sylvester@archis.company>
2023-10-09 09:37:10 +0300

    Untracked and ignored .contentlayer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
+import { useLiveReload } from "next-contentlayer/hooks";
 import type { AppProps } from "next/app";
 import Layout from "@components/Layout";
 import "../styles/main.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useLiveReload();
+
   return (
     <Layout>
       <Component {...pageProps} />
