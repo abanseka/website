6932a9338373a2eedbc100354732062626b23b81
sly <sylvester@archis.company>
2024-10-18 13:36:07 +0100

    fix broken codeblock formatting

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -3,9 +3,10 @@ import style from "./footer.module.scss";
 
 const Footer = () => (
   <footer className={style.footer}>
-    <hr />
     <br />
+
     <Navlinks renderIcon={false} />
+
     <p className={style.footertext}>
       Copyright © {new Date().getFullYear()} abanseka. All rights reserved.
     </p>
