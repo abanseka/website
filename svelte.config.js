d253d03df1033bb0671a0fbb33548fc247982f43
sly <sylvester@archis.company>
2026-02-27 23:57:07 +0100

    switch to adapter-static for cloudflare pages compatibility

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,18 @@
-import adapter from "@sveltejs/adapter-cloudflare";
+import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
-  kit: {
-    adapter: adapter(),
-  },
+	preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter({
+			pages: 'dist',
+			assets: 'dist',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		})
+	}
 };
 
 export default config;
